aff678a58429f839fa7b806f25ee0aed0fe5be5c
russjohnson09 <russjohnson09@gmail.com>
2013-01-16 08:53:57 -0500

    update

--- assignment1/1.cpp
+++ assignment1/1.cpp
@@ -14,7 +14,7 @@ int main(int argc, const char* argv[])
     int sum = 0;
     for ( int i = 0 ;  i < n ;  i++ ) {
         for ( int j = 0 ;  j < 100 ;  j++ ) {
-                sum++;
+            sum++;
         }
     }
 
