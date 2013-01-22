86bd810ddcc56bd5d747cc867e3ed078b41ed17b
Russ <russjohnson09@gmail.com>
2013-01-22 00:05:27 -0500

    update

--- assignment3/dvd.cpp
+++ assignment3/dvd.cpp
@@ -7,7 +7,7 @@ DVD::DVD(const string t, int m, double p) : title(t), minutes(m), price(p) {
 }
 
 void DVD::output() const {
-	cout << "Title: " << get_title() << '\n' << "Minutes: " << get_minutes() << '\n' << "Price: " << get_price() << '\n';
+	cout << "Title: " << title << '\n' << "Minutes: " << minutes << '\n' << "Price: " << price << '\n';
 }
 
 void DVD::changeprice(double p_new) {
