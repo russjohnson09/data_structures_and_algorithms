ca087499bd8d50226e2e53dd9967858aab7b0c45
russjohnson09 <russjohnson09@gmail.com>
2013-02-10 18:08:37 -0500

    u

---
+++ assignment6/tree/BinarySearchTree.h
@@ -0,0 +1,92 @@
+#ifndef BINARY_SEARCH_TREE_H_
+#define BINARY_SEARCH_TREE_H_
+
+  // Binary node and forward declaration because g++ does
+  // not understand nested classes.
+template <class Comparable>
+class BinarySearchTree;
+
+template <class Comparable>
+class BinaryNode
+{
+    Comparable element;
+    BinaryNode *left;
+    BinaryNode *right;
+
+  public:
+    BinaryNode( const Comparable & theElement, BinaryNode *lt, BinaryNode *rt )
+      : element( theElement ), left( lt ), right( rt ) { }
+  friend class BinarySearchTree<Comparable>;
+};
+
+
+// BinarySearchTree class
+//
+// CONSTRUCTION: with ITEM_NOT_FOUND object used to signal failed finds
+//
+// ******************PUBLIC OPERATIONS*********************
+// void insert( x )       --> Insert x
+// void remove( x )       --> Remove x
+// Comparable find( x )   --> Return item that matches x
+// Comparable findMin( )  --> Return smallest item
+// Comparable findMax( )  --> Return largest item
+// boolean isEmpty( )     --> Return true if empty; else false
+// void makeEmpty( )      --> Remove all items
+// void printTree( )      --> Print tree in sorted order
+
+template <class Comparable>
+class BinarySearchTree
+{
+  public:
+    explicit BinarySearchTree( const Comparable & notFound );
+    BinarySearchTree( const BinarySearchTree & rhs );
+    BinarySearchTree( BinaryNode<Comparable> * root_given, const Comparable & notFound );
+    ~BinarySearchTree( );
+
+    const Comparable & findMin( ) const;
+    const Comparable & findMax( ) const;
+    const Comparable & find( const Comparable & x ) const;
+    bool isEmpty( ) const;
+    void printTree_inorder( ) const;
+    void printTree_postorder( ) const;
+    int height( ) const;
+
+    void makeEmpty( );
+    void insert( const Comparable & x );
+    void remove( const Comparable & x );
+
+    const BinarySearchTree & operator=( const BinarySearchTree & rhs );
+
+    void printTree_preorder() const;
+    int nodes() const;
+    int leaves() const;
+    int fullNodes() const;
+
+
+
+  private:
+    BinaryNode<Comparable> *root;
+    const Comparable ITEM_NOT_FOUND;
+
+    const Comparable & elementAt( BinaryNode<Comparable> *t ) const;
+
+    void insert( const Comparable & x, BinaryNode<Comparable> * & t ) const;
+    void remove( const Comparable & x, BinaryNode<Comparable> * & t ) const;
+    BinaryNode<Comparable> * findMin( BinaryNode<Comparable> *t ) const;
+    BinaryNode<Comparable> * findMax( BinaryNode<Comparable> *t ) const;
+    BinaryNode<Comparable> * find( const Comparable & x, BinaryNode<Comparable> *t ) const;
+    void makeEmpty( BinaryNode<Comparable> * & t ) const;
+    void printTree_inorder( BinaryNode<Comparable> *t ) const;
+    void printTree_postorder( BinaryNode<Comparable> *t ) const;
+    int height( BinaryNode<Comparable> *t ) const;
+    BinaryNode<Comparable> * clone( BinaryNode<Comparable> *t ) const;
+
+    void printTree_preorder( const BinaryNode<Comparable> * t ) const;
+    int nodes( const BinaryNode<Comparable> * t ) const;
+    int leaves( const BinaryNode<Comparable> * t ) const;
+    int fullNodes( const BinaryNode<Comparable> * t ) const;
+
+};
+
+#include "BinarySearchTree.cpp"
+#endif
---
+++ assignment6/tree/BinaryTreeDemo.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "BinarySearchTree.h"
+
+using namespace std;
+
+void printTime(double);
+
+BinarySearchTree<int> balanced(int);
+
+int
+main()
+{
+
+	clock_t t;
+    int i;
+
+
+
+    BinarySearchTree<string> bt
+        (
+        new BinaryNode<string>("Lee",
+                new BinaryNode<string>("Christie",
+                        new BinaryNode<string>("Russ",NULL,NULL),
+                        new BinaryNode<string>("Andrew",NULL,NULL)
+                        ),
+                new BinaryNode<string>("Tom",
+                        new BinaryNode<string>("Conner",NULL,NULL),
+                        new BinaryNode<string>("Ellie",NULL,NULL)
+                        )
+                ),
+        ""
+        );
+
+    BinarySearchTree<string> bt2
+        (
+        new BinaryNode<string>("Russ",
+                new BinaryNode<string>("Christie",
+                        new BinaryNode<string>("Andrew",NULL,NULL),
+                        new BinaryNode<string>("Lee",
+                            new BinaryNode<string>("Conner",NULL,
+                                new BinaryNode<string>("Ellie",NULL,NULL)),
+                            NULL)
+                        ),
+                new BinaryNode<string>("Tom",NULL,NULL)
+                ),
+        ""
+        );
+
+    BinarySearchTree<string> bt3
+        (
+        new BinaryNode<string>("A",
+            new BinaryNode<string>("B",
+                new BinaryNode<string>("D",NULL,NULL),
+                new BinaryNode<string>("E",NULL,NULL)
+                ),
+            new BinaryNode<string>("C",
+                new BinaryNode<string>("F",NULL,NULL),
+                NULL
+            )
+        ),
+    ""
+    );
+
+    BinarySearchTree<string> bt4
+        (
+        new BinaryNode<string>("D",
+            new BinaryNode<string>("B",
+                new BinaryNode<string>("A",NULL,NULL),
+                new BinaryNode<string>("C",NULL,NULL)
+                ),
+            new BinaryNode<string>("F",
+                new BinaryNode<string>("E",NULL,NULL),
+                NULL
+            )
+        ),
+    ""
+    );
+
+    BinarySearchTree<string> bt5
+        (
+        new BinaryNode<string>("D",
+            new BinaryNode<string>("G",
+                new BinaryNode<string>("A",NULL,NULL),
+                new BinaryNode<string>("B",NULL,NULL)
+                ),
+            new BinaryNode<string>("F",
+                new BinaryNode<string>("E",NULL,NULL),
+                NULL
+            )
+        ),
+    ""
+    );
+
+    BinarySearchTree<string> bt6
+        (
+        new BinaryNode<string>("D",
+            new BinaryNode<string>("B",
+                new BinaryNode<string>("A",NULL,NULL),
+                new BinaryNode<string>("G",NULL,NULL)
+                ),
+            new BinaryNode<string>("F",
+                new BinaryNode<string>("E",NULL,NULL),
+                NULL
+            )
+        ),
+    ""
+    );
+
+    BinarySearchTree<string> bt7
+        (
+        new BinaryNode<string>("D",
+            new BinaryNode<string>("C",
+                new BinaryNode<string>("A",NULL,NULL),
+                new BinaryNode<string>("B",NULL,NULL)
+                ),
+            new BinaryNode<string>("F",
+                new BinaryNode<string>("E",NULL,NULL),
+                NULL
+            )
+        ),
+    ""
+    );
+
+    bt.printTree_inorder();
+    cout << '\n';
+    bt2.printTree_inorder();
+    cout << '\n';
+    bt3.printTree_inorder();
+    cout << '\n';
+
+    bt4.printTree_inorder();
+    cout << '\n';
+    bt5.printTree_inorder();
+    cout << '\n';
+    bt6.printTree_inorder();
+    cout << '\n';
+    bt7.printTree_inorder();
+    cout << '\n';
+
+
+}
+
+
+void printTime(double t) {
+   printf("Time in Seconds for 1000 executions: %lf\n",t/CLOCKS_PER_SEC);
+}
